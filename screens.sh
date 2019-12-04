b4fbf10f47b5edd017a764eb9d74024e42ff7577
Cancain <eriksson.tomas@gmail.com>
2019-12-04 12:07:18 +0100

    Init

---
+++ screens.sh
@@ -0,0 +1,1 @@
+xrandr --auto && xrandr --output DVI-I-2-2 --left-of eDP-1 && xrandr --output DVI-I-1-1 --right-of eDP-1
---
+++ touchpad.sh
@@ -0,0 +1,1 @@
+sh -c 'echo -n "elantech" > /sys/bus/serio/devices/serio1/protocol'
