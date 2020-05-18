35394c9daa8ebf1e0d27ea463be12498219f44b3
Cancain <eriksson.tomas@gmail.com>
2020-05-18 18:46:50 +0200

    added alternate executable for debugging with dbg

--- cppstart/cppstart.py
+++ cppstart/cppstart.py
@@ -22,14 +22,15 @@ def create_cpp_main():
     template = headers + main_function
     main_cpp.write(template);
 
-def create_build():
+def create_executables():
     os.system("cp ~/scripts/cppstart/run.py %s" %current_folder)
+    os.system("cp ~/scripts/cppstart/debug.py %s" %current_folder)
 
 def main():
     make_folder("src")
     make_folder("build")
     create_cpp_main()
-    create_build()
+    create_executables()
 
 if __name__ == "__main__":
     main()
